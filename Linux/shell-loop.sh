113a50a89bdfc94058d085faff9a719ccde9cb88
Lakshay Goel <39944568+lakshaygoel20@users.noreply.github.com>
2021-02-19 18:06:28 +0530

    Add files via upload

---
+++ Linux/shell-loop.sh
@@ -0,0 +1,21 @@
+# Linux Practice
+#Name of module --- Linux Basic Loops practice
+#Date : 18th feb 2021 10:00:PM
+#Author: Lakshay
+#Modification History : None 
+#Functions -- None
+#Global Variables--- 
+
+
+for i in {0..10..2}
+  do 
+     echo "Welcome $i times"
+ done
+
+
+names='Stan Kyle Cartman'
+for name in $names
+do
+echo $name
+done
+echo All done
